4723cba700570a8b186ea1360e8d5befabce7c38
Oleksii <37875781+hm2thr33@users.noreply.github.com>
2023-05-31 17:54:02 +0300

    add ERROR_MSG send to Slack

--- postgres-backup-s3/backup.sh
+++ postgres-backup-s3/backup.sh
@@ -3,7 +3,7 @@
 set -e
 set -o pipefail
 
-BAK_DATETIME=`date +%F-%H%M`
+BAK_DATETIME=$(date +%F-%H%M)
 
 if [ "${S3_ACCESS_KEY_ID}" = "**None**" ]; then
   echo "You need to set the S3_ACCESS_KEY_ID environment variable."
@@ -52,7 +52,12 @@ else
 fi
 
 if [ "${PASSPHRASE}" = "**None**" ]; then
-  echo "You need to set the POSTGRES_DATABASE environment variable."
+  echo "You need to set the PASSPHRASE environment variable."
+  exit 1
+fi
+
+if [ "${SLACK_WEBHOOK}" = "**None**" ]; then
+  echo "You need to set the SLACK_WEBHOOK environment variable."
   exit 1
 fi
 
@@ -68,12 +73,24 @@ POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTG
 
 echo "Creating dump of ${POSTGRES_DATABASE} database from ${POSTGRES_HOST}..."
 
-pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE > ${POSTGRES_DATABASE}-${BAK_DATETIME}.dump.sql
+if ! pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE > ${POSTGRES_DATABASE}-${BAK_DATETIME}.dump.sql 2> /tmp/pgdump_error.log; then
+  ERROR_MSG=$(cat /tmp/pgdump_error.log)
+  echo "Backup process failed with error: $ERROR_MSG. Sending message to Slack..."
+  curl -X POST -H 'Content-type: application/json' --data '{"text": ":exclamation: Backup process for '${POSTGRES_DATABASE}' failed with error: ```'"$ERROR_MSG"'```"}' $SLACK_WEBHOOK
+  rm /tmp/pgdump_error.log
+  exit 1
+fi
+
+rm /tmp/pgdump_error.log
+
 7z a dump.sql.7z -t7z -m0=lzma2:d1024m -mx=9 -aoa -mfb=64 -md=32m -p"$PASSPHRASE" -ms=on ${POSTGRES_DATABASE}-${BAK_DATETIME}.dump.sql
 
 echo "Uploading dump to $S3_BUCKET"
 
 s3cmd $AWS_ARGS put dump.sql.7z s3://$S3_BUCKET/$S3_PREFIX/${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.7z || exit 2
+
+# send alert to slack
+curl -X POST -H 'Content-type: application/json' --data '{"text":":approved: POSTGRES backup - '${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ")' uploaded successfully!"}' $SLACK_WEBHOOK
 
 rm -f ${POSTGRES_DATABASE}-${BAK_DATETIME}.dump.sql && rm -f dump.sql.7z
 
